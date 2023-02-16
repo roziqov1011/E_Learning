592945f6066c88fa3b7fb4a424acf2d3fbb6a1ef
Asadbek-pro567 <masadbek2006@gmail.com>
2023-02-16 18:06:39 +0500

    home header

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.scss'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +15,49 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1>Home</h1>
-      </main>
+      <div className={styles.home}>
+        <div className={styles.container}>
+          <div className={styles.home__inner}>
+            <div>
+              <h1>Studying Online is now much easier</h1>
+              <p>TOTC is an interesting platform that will teach you in more an interactive way</p>
+              <span>
+                <Link href=''>
+                  <button>Join free</button>
+                </Link>
+                <Link href=''>
+                  <button>
+                    <h5>
+                      <Image src='/image/play__foto.svg' alt='play foto' width={40} height={40}/>
+                    </h5>
+                    <h6>Watch how it works</h6>
+                  </button>
+                </Link>
+              </span>
+            </div>
+            
+            <ul>
+              <li>
+                <h6>250K</h6>
+                <p>Assisted Student</p>
+              </li>
+              <li>
+                <h6>250K</h6>
+                <p>Assisted Student</p>
+              </li>
+              <li>
+                <h6>250K</h6>
+                <p>Assisted Student</p>
+              </li>
+              <li>
+                <h6>250K</h6>
+                <p>Assisted Student</p>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
