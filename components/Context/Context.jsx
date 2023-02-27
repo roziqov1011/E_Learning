e10ac2d35b0e6267b11ed1658681bc954e4340f9
Obd2004 <obidjonabd1097@gmail.com>
2023-02-27 16:34:25 +0500

    123

--- components/Context/Context.jsx
+++ components/Context/Context.jsx
@@ -3,8 +3,7 @@ import { createContext, useState } from "react";
 
 const Context = createContext()
 function Provider({children}){
-    const wind = window.localStorage.getItem('key')
-    const [order, setOrder] = useState(wind !== 'true' ? false : true)
+    const [order, setOrder] = useState(false)
     
     return (
         <Context.Provider value={{order, setOrder}}>
